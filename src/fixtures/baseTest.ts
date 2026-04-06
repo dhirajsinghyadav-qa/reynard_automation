55b2a5210d2cc43983afa27a1f4caafefb6ce393
Dhirajsingh Yadav <dhirajsingh.yadav@kiwiqa.com>
2026-04-06 10:22:14 +0530

    Change the baseTest.ts file logic

--- src/fixtures/baseTest.ts
+++ src/fixtures/baseTest.ts
@@ -1,3 +1,36 @@
+/* import { STORAGE_STATE_PATH } from '../../global-setup';
+import { test as base } from '@playwright/test';
+import fs from 'fs';
+
+// ✅ Custom fixture — storageState aware context
+export const test = base.extend({
+  page: async ({ browser }, use, testInfo) => {
+    // ── Login tests ke liye → fresh context (no storageState) ──
+    const isLoginTest = testInfo.file.includes('login.spec');
+
+    const contextOptions = isLoginTest
+      ? {
+          permissions: [], // 🔥 block notification popup
+        }
+      : {
+          permissions: [],
+          // ✅ storageState inject — agar file exist kare to
+          ...(fs.existsSync(STORAGE_STATE_PATH) && {
+            storageState: STORAGE_STATE_PATH,
+          }),
+        };
+
+    const context = await browser.newContext(contextOptions);
+    const page = await context.newPage();
+
+    await use(page);
+
+    await context.close();
+  },
+});
+
+export const expect = test.expect; */
+
 /* import { test as base } from '@playwright/test';
 
 // ✅ Custom fixture (ONLY context control)
@@ -25,20 +58,15 @@ import fs from 'fs';
 // ✅ Custom fixture — storageState aware context
 export const test = base.extend({
   page: async ({ browser }, use, testInfo) => {
-    // ── Login tests ke liye → fresh context (no storageState) ──
     const isLoginTest = testInfo.file.includes('login.spec');
 
-    const contextOptions = isLoginTest
-      ? {
-          permissions: [], // 🔥 block notification popup
-        }
-      : {
-          permissions: [],
-          // ✅ storageState inject — agar file exist kare to
-          ...(fs.existsSync(STORAGE_STATE_PATH) && {
-            storageState: STORAGE_STATE_PATH,
-          }),
-        };
+    const contextOptions: any = {
+      permissions: [],
+    };
+
+    if (!isLoginTest && fs.existsSync(STORAGE_STATE_PATH)) {
+      contextOptions.storageState = STORAGE_STATE_PATH;
+    }
 
     const context = await browser.newContext(contextOptions);
     const page = await context.newPage();
